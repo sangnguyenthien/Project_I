f669a90e484e723c8dca47a5fc326c05a095d648
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-06-16 21:56:12 +0700

    Create GetUserInfo.java: Get User Information

---
+++ UserInfo/GetUserInfo.java
@@ -0,0 +1,33 @@
+package UserInfo;
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class GetUserInfo {
+
+    //private static final String AUTH_TOKEN = "Bearer eyJ0eXAiO ... 0X2tnSQLEANnSPHY0gKcgw";
+    private static final String GRAPH_API_ENDPOINT = "https://graph.microsoft.com/v1.0/users";
+
+    public static void main(String[] args) throws IOException {
+        //String response = makeGraphApiRequest();
+        //System.out.println(response);
+    }
+
+    private static String makeGraphApiRequest(String accessToken) throws IOException {
+        String AUTH_TOKEN = "Bearer " + accessToken;
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet(GRAPH_API_ENDPOINT);
+        request.setHeader("Authorization", AUTH_TOKEN);
+
+        HttpResponse httpResponse = httpClient.execute(request);
+        String response = EntityUtils.toString(httpResponse.getEntity());
+
+        httpClient.close();
+
+        return response;
+    }
+}
