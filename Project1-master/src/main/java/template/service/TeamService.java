b8e5720f0052800c6d7058c0fa8bf77ba43b8984
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-07-08 14:42:24 +0700

    Update TeamService.java

--- Project1-master/src/main/java/template/service/TeamService.java
+++ Project1-master/src/main/java/template/service/TeamService.java
@@ -1,14 +1,26 @@
 package template.service;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 
@@ -16,6 +28,7 @@ import org.apache.http.util.EntityUtils;
 import template.api_config.config;
 
 public class TeamService {
+
     public static void create(String group_id) throws IOException, InterruptedException
     {
         String accessToken = config.getAccessToken();
@@ -75,10 +88,68 @@ public class TeamService {
         }
     }
 
+    public static List<JsonObject> listUsersAsJson(String group_id, String token) throws IOException, InterruptedException
+    {
+        String graphEndpoint = "https://graph.microsoft.com/v1.0/teams/"+ group_id +"/members";
+        HttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+        try{
+            HttpGet httpPost = new HttpGet(graphEndpoint);
+            httpPost.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+            HttpResponse response = httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                System.out.println("Cannot list users of Team has id: " +group_id);
+                return null;
+            }
+
+            String body =  EntityUtils.toString(response.getEntity());
+            JsonObject bodyJson = JsonParser.parseString(body).getAsJsonObject();
+
+            JsonArray jsonArray = new JsonArray();
+
+            if (bodyJson.has("value"))
+            {
+                jsonArray = bodyJson.get("value").getAsJsonArray();
+            }
+
+            List<JsonObject> result = new ArrayList<>();
+
+            for(int i=0; i< jsonArray.size(); i++)
+            {
+                JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+
+                // Get the value of "roles" as a comma-separated String
+                String rolesString = jsonObject.get("roles").getAsJsonArray().toString();
+
+                // Update the JsonObject with the new value of "roles"
+                jsonObject.addProperty("roles", rolesString);
+
+                result.add(jsonObject);
+            }
+
+            return result;
+
+
+
+        }catch(IOException e)
+        {
+            System.out.println("Cannot list users of Team has id: " +group_id);
+            return null;
+        }
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException
     {
         String group_id = "7e8dcfe0-1fd3-4226-aeb0-63e1cc394c39";
-        TeamService.create(group_id);
+        String token = config.getAccessToken();
+        List<JsonObject> abc = TeamService.listUsersAsJson(group_id, token);
+        for(JsonObject j : abc)
+        {
+            System.out.println(j.toString());
+        }
     }
 
     
