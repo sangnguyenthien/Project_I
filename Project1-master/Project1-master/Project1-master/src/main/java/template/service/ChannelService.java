2830006473ebdb1e1c59f4759da6240036b7636c
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-07-08 23:03:54 +0700

    Add files via upload

---
+++ Project1-master/Project1-master/Project1-master/src/main/java/template/service/ChannelService.java
@@ -0,0 +1,69 @@
+package template.service;
+
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import template.team_config.config;
+
+
+import java.io.IOException;
+
+
+public class ChannelService {
+    public static void create(String teamId, String displayName, String description) throws IOException, InterruptedException
+    {
+        //String teamId = "7e8dcfe0-1fd3-4226-aeb0-63e1cc394c39";
+        String membershipType = "standard";
+        String url = "https://graph.microsoft.com/v1.0/teams/" + teamId + "/channels";
+        String body = String.format("{\n" +
+                "  \"displayName\": \"%s\",\n" +
+                "  \"description\": \"%s\",\n" +
+                "  \"membershipType\": \"%s\"\n" +
+                "}", displayName, description, membershipType);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(url);
+
+        // Set request headers
+        request.setHeader("Content-type", "application/json");
+
+
+        // Set the Authorization header with the access token
+        String accessToken = config.getAccessToken();
+        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+
+        // Set request body
+        StringEntity params = new StringEntity(body);
+        request.setEntity(params);
+
+        // Send request
+        HttpResponse response = httpClient.execute(request);
+
+        if (response.getStatusLine().getStatusCode() == 201) {
+            System.out.println("Created channel " + displayName + " successfully!");
+            /*
+            // Read response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer result = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            // Print response
+            System.out.println("Response code: " + response.getStatusLine().getStatusCode());
+            System.out.println("Response body: " + result.toString());
+            */
+        }else{
+            System.out.printf("Failed to create channel. Status code: %d, Error message: %s", response.getStatusLine().getStatusCode(), response.getEntity().getContent().toString());
+        }
+    }
+
+}
+
+
---
+++ Project1-master/Project1-master/Project1-master/src/main/java/template/service/airtable/Field.java
@@ -0,0 +1,121 @@
+package template.service.airtable;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.google.gson.JsonObject;
+
+public class Field {
+    private final String id;
+    private final String name;
+    private final String type;
+
+    protected Field(JsonObject field)
+    {
+        this.id = field.get("id").getAsString();
+        this.name = field.get("name").getAsString();
+        this.type = field.get("type").getAsString();
+    }
+
+    protected String getId()
+    {
+        return this.id;
+    }
+
+    protected String getName()
+    {
+        return this.name;
+    }
+
+    protected String getType()
+    {
+        return this.type;
+    }
+
+    protected static String createField(JsonObject field, String baseId, String tableId, String personal_access_token)
+    {
+        String endpoint = "https://api.airtable.com/v0/meta/bases/" + baseId + "/tables/" + tableId + "/fields";
+        HttpClient client = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+
+        try
+        {        
+            HttpPost httpPost = new HttpPost(endpoint);
+            httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            httpPost.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + personal_access_token);
+
+            httpPost.setEntity(new StringEntity(field.toString()));
+
+            HttpResponse response = client.execute(httpPost);
+
+            if (response.getStatusLine().getStatusCode() == 200)
+            {
+                System.out.println("Created field successfully");
+                HttpEntity entity = response.getEntity();
+                String responseString = EntityUtils.toString(entity);
+                return responseString;
+            }
+            else{
+                System.out.println("Error code: " + response.getStatusLine().getStatusCode());
+                return response.toString();
+            }
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+
+
+    }
+
+    protected static String updateField(JsonObject field, String fieldId, String baseId, String tableId, String personal_access_token)
+    {
+        String endpoint = "https://api.airtable.com/v0/meta/bases/" + baseId + "/tables/" + tableId + "/fields/" + "fieldId";
+        HttpClient client = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+
+        try
+        {        
+            HttpPost httpPost = new HttpPost(endpoint);
+            httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            httpPost.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + personal_access_token);
+
+            httpPost.setEntity(new StringEntity(field.toString()));
+
+            HttpResponse response = client.execute(httpPost);
+
+            if (response.getStatusLine().getStatusCode() == 200)
+            {
+                System.out.println("Updated field successfully");
+                HttpEntity entity = response.getEntity();
+                String responseString = EntityUtils.toString(entity);
+                return responseString;
+            }
+            else{
+                System.out.println("Error code: " + response.getStatusLine().getStatusCode());
+                return response.toString();
+            }
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+}
