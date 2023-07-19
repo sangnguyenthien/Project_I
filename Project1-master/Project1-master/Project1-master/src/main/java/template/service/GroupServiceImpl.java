60f9095769cae8bfe2c81ff1df43716f2995f4cc
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-07-19 23:47:11 +0700

    Update GroupServiceImpl.java
    Add feature: list existed groupIDs

--- Project1-master/Project1-master/Project1-master/src/main/java/template/service/GroupServiceImpl.java
+++ Project1-master/Project1-master/Project1-master/src/main/java/template/service/GroupServiceImpl.java
@@ -7,14 +7,13 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import template.persistence.dto.Group;
 
@@ -300,6 +299,51 @@ public class GroupServiceImpl implements GroupService {
         } catch (IOException e) {
             System.out.println("Cannot list users of Team has id: " + group_id);
             return null;
+        }
+    }
+
+    public void listIDsGroup() throws IOException, InterruptedException
+    {
+        String accessToken = token;
+
+        // Set the request headers
+        HttpGet request = new HttpGet("https://graph.microsoft.com/v1.0/groups");
+        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        try {
+            // Send the request and get the response
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            // Check the response status code
+            if (response.getStatusLine().getStatusCode() == 200) {
+                // Get the response body as a string
+                String responseBody = EntityUtils.toString(response.getEntity());
+
+                // Parse the JSON response
+                JSONObject responseJson = new JSONObject(responseBody);
+
+                // Get the array of groups from the response
+                JSONArray groupsJson = responseJson.getJSONArray("value");
+
+                // Print the IDs of the groups
+                for (int i = 0; i < groupsJson.length(); i++) {
+                    JSONObject groupJson = groupsJson.getJSONObject(i);
+                    String groupId = groupJson.getString("id");
+                    System.out.println(groupId);
+                }
+            } else {
+                System.out.printf("Failed to list groups. Status code: %d, Error message: %s", response.getStatusLine().getStatusCode(), response.getEntity().getContent().toString());
+            }
+
+            // Close the response and the HttpClient
+            response.close();
+            httpClient.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Failed to list groups");
         }
     }
 
