d5066bc4c0c4fd48e6a4b8363f1a27679549f3f5
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-07-23 10:19:21 +0700

    Update Color.java

--- Project1-master/Project1-master/Project1-master/src/main/java/template/colorUtil/Color.java
+++ Project1-master/Project1-master/Project1-master/src/main/java/template/colorUtil/Color.java
@@ -2,28 +2,27 @@ package template.colorUtil;
 
 import org.fusesource.jansi.Ansi;
 public class Color {
-    public static void print_red(String text)
+    public static void printRed(String text)
     {
         System.out.println(Ansi.ansi().fgRed().a(text).reset());
     }
 
-    public static void print_green(String text)
+    public static void printGreen(String text)
     {
         System.out.println(Ansi.ansi().fgGreen().a(text).reset());
     }
 
-    public static void print_yellow(String text)
+    public static void printYellow(String text)
     {
         System.out.println(Ansi.ansi().fgYellow().a(text).reset());
     }
 
-    public static void print_yellow_no(String text)
+    public static void printYellowNo(String text)
     {
         System.out.print(Ansi.ansi().fgYellow().a(text).reset());
     }
-
-
-    public static void print_blue(String text)
+    
+    public static void printBlue(String text)
     {
         System.out.println(Ansi.ansi().fgBlue().a(text).reset());
     }
