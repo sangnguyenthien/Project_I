40011be26524001ea3bb85cbbe4a32760980166e
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-07-31 10:53:24 +0700

    Update TaskScheduleForAntiRicons.java

--- Project1-master/Project1-master/Project1-master/src/main/java/TaskScheduleForAntiRicons.java
+++ Project1-master/Project1-master/Project1-master/src/main/java/TaskScheduleForAntiRicons.java
@@ -8,26 +8,32 @@ import java.util.List;
 
 public class TaskScheduleForAntiRicons {
     public static void main(String[] args) throws Exception{
-        String configAirTable = Config.getConfigAirTable();
-        //Change the value of variable infoTaskSchedule to absolute path of infoTaskSchedule.json and start using
-        final String infoTaskSchedule = "D:\\java prj1\\Project1-master\\Project1-master\\Project1-master\\src\\main\\java\\infoTaskSchedule.json";
-        
-        String token = Config.getAccessToken();
-        String tableId = JsonTool.getAccessInfo(infoTaskSchedule).get("tableId").getAsString();
-        String groupId = JsonTool.getAccessInfo(infoTaskSchedule).get("groupId").getAsString();
+        try {
+            String configAirTable = Config.getConfigAirTable();
+            //Change the value of variable infoTaskSchedule to absolute path of infoTaskSchedule.json and start using
+            final String infoTaskSchedule = "C:\\Users\\admin\\Downloads\\Project_I-main\\Project1-master\\Project1-master\\Project1-master\\src\\main\\java\\infoTaskSchedule.json";
 
-        List<JsonObject> fields = GroupServiceImpl.listUsersAsJson(groupId, token);
-        //token Airtable
-        JsonObject access = JsonTool.getAccessInfo(configAirTable).getAsJsonObject();
-        String personal_access_token = access.get("personal_access_token").getAsString();
-        String baseId = access.get("baseId").getAsString();
+            String token = Config.getAccessToken();
 
-        JsonObject fieldTable = Table.getTable(tableId, baseId, personal_access_token);
 
-        Table table = new Table(fieldTable, baseId, personal_access_token);
-        table.pullAllRecords(fields, baseId, personal_access_token);
+            String tableId = JsonTool.getAccessInfo(infoTaskSchedule).get("tableId").getAsString();
+            String groupId = JsonTool.getAccessInfo(infoTaskSchedule).get("groupId").getAsString();
+
+            List<JsonObject> fields = GroupServiceImpl.listUsersAsJson(groupId, token);
+            //token Airtable
+            JsonObject access = JsonTool.getAccessInfo(configAirTable).getAsJsonObject();
+            String personal_access_token = access.get("personal_access_token").getAsString();
+            String baseId = access.get("baseId").getAsString();
+
+            JsonObject fieldTable = Table.getTable(tableId, baseId, personal_access_token);
+
+            Table table = new Table(fieldTable, baseId, personal_access_token);
+            table.pullAllRecords(fields, baseId, personal_access_token);
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Syntax Error");
+        }
 
     }
-
-
 }
